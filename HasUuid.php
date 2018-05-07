fc69c94246c49576283c4e228b5db8ebaf37221f
Nazarii_Makar <nazar.makar@magnise.com>
2018-05-07 14:34:59 +0300

    Init

---
+++ HasUuid.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace EFrame\Uuid;
+
+use Ramsey\Uuid\Uuid;
+
+class HasUuid
+{
+    /**
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * This function overwrites the default boot static method of Eloquent models. It will hook
+     * the creation event with a simple closure to insert the UUID
+     */
+    public static function bootUuidModelTrait()
+    {
+        static::creating(function ($model) {
+
+            if (!isset($model->attributes[$model->getKeyName()])) {
+                $model->incrementing = false;
+                $model->attributes[$model->getKeyName()] = Uuid::uuid4()->toString();
+            }
+        }, 0);
+    }
+}
